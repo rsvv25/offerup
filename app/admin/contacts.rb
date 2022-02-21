e926308871c26f0368edc65bd3b465ad1286c594
anonymous anonymous <richard.vargo@kellogg.northwestern.edu>
2022-02-21 03:14:12 +0000

    Whitelist columns in Contact for ActiveAdmin

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Contact do
 
+ permit_params :bought_id, :selling_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
