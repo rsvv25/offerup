5eb6d5a8d95a3c6cd95ac8f8067ecf83c9969612
anonymous anonymous <richard.vargo@kellogg.northwestern.edu>
2022-02-21 03:14:00 +0000

    Generate contacts resource with ideas:scaffold

---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    bought_id
+  end
+
+end
---
+++ app/views/contacts/_contact.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! contact, :id, :bought_id, :selling_id, :created_at, :updated_at
+json.url contact_url(contact, format: :json)
